50bbfacdb4ed8ffc68ba58f96a14e7a7de60fe1e
guozhicheng <guozhicheng@piesat.cn>
2020-08-26 18:02:12 +0800

    完成注册功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -8,31 +8,33 @@ Page({
    * 页面的初始数据
    */
   data: {
+    isLogin:true,
     // 登录
     userName:'',
-    userPlaceholder:'请输入用户账号',
+    userPlaceholder: '请输入手机号',
     passName:'',
-    passPlaceholder:'请输入用户密码',
+    passPlaceholder:'请输入密码',
     // 注册
     regUserName:'',
     regUserPlaceholder: '请输入手机号',
+    regVerify:'',
+    regVerUserPlaceholder:'请填写6位验证码',
     regPassName:'',
     regPassPlaceholder:'请输入6到18位密码',
     regPassNameAgn:'',
     regPassPlaceholderAgn:'请再次输入密码',
     // 分割线
     vanDivider:{ color: '#FFF', borderColor: '#F00', padding: '0 16px' },
-
+    // 胶囊高度
+    topVal:0,
+    heightVal:0,
+    // 验证码按钮内容
+    verifyButtenText:'发送验证码',
+    sending:true,
   },
   /**
    * 微信登陆小程序
    */
-  userChange:function(event){
-    this.data.userName=event.detail;
-  },
-  pwdChange:function(event){
-    this.data.passName=event.detail;
-  },
   loginByWX:function(){
     wx.login({
       success (res) {
@@ -88,7 +90,7 @@ Page({
   },
   dologin:function(){
     if(this.data.userName===''){
-      Toast.fail('请输入账号');
+      Toast.fail('请输入手机号');
       return;
     }else if(this.data.passName===''){
       Toast.fail('请输入密码');
@@ -126,6 +128,147 @@ Page({
       }
     })
   },
+  // 注册手机号
+  regUserFocus: function () {
+    this.setData({
+      regUserPlaceholder: ''
+    })
+  },
+  regUserBlur: function () {
+    if (this.data.regUserName === '') {
+      this.setData({
+        regUserPlaceholder: '请输入手机号'
+      })
+    }
+  },
+  // 注册验证码
+  regVerUserFocus: function () {
+    this.setData({
+      regVerUserPlaceholder: ''
+    })
+  },
+  regVerUserBlur: function () {
+    if (this.data.regVerify === '') {
+      this.setData({
+        regVerUserPlaceholder: '请填写6位验证码'
+      })
+    }
+  },
+  // 注册密码
+  regPassFocus: function () {
+    this.setData({
+      regPassPlaceholder: ''
+    })
+  },
+  regPassBlur: function () {
+    if (this.data.regPassName === '') {
+      this.setData({
+        regPassPlaceholder: '请输入6到18位密码'
+      })
+    }
+  },
+  // 注册确认密码
+  regPassFocusAgn: function () {
+    this.setData({
+      regPassPlaceholderAgn: ''
+    })
+  },
+  regPassBlurAgn: function () {
+    if (this.data.regPassNameAgn === '') {
+      this.setData({
+        regPassPlaceholderAgn: '请再次输入密码'
+      })
+    }
+  },
+  doReg:function(){
+    if (this.data.regUserName === '') {
+      Toast.fail('请输入手机号');
+      return;
+    } else if (this.data.regVerify === '') {
+      Toast.fail('请输入验证码');
+      return;
+    } else if (this.data.regPassName === '') {
+      Toast.fail('请输入密码');
+      return;
+    } else if (this.data.regPassNameAgn === '') {
+      Toast.fail('请确认密码');
+      return;
+    } else if (this.data.regPassNameAgn !== this.data.regPassName) {
+      Toast.fail('两次输入密码不一致');
+      return;
+    }
+    wx.request({
+      url: 'http://127.0.0.1:888/reg',
+      data: {
+        phone: this.data.regUserName,
+        upwd: this.data.regPassName,
+        email: this.data.regVerify,
+        uname: '测试用户'
+      },
+      success: res => {
+        if (res.data.code == -1) {
+          console.log(res)
+          // Toast.fail(res.data.msg);
+          // Notify("\n\n"+res.data.msg+"\n");
+          Notify({
+            message: "\n\n" + res.data.msg + "\n",
+            color: '#ad0000',
+            background: '#ffe1e1',
+            duration: 2500,
+          });
+          return;
+        } else if (res.data.code == 1) {
+          // Toast.fail(res.data.msg);
+          Toast.fail({
+            type: 'success',
+            message: res.data.msg,
+            duration: 2000
+          });
+          this.setData({
+            isLogin: true
+          })
+        }
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
+  },
+  goReg:function(){
+    this.setData({
+      isLogin:false
+    })
+  },
+  goLogin: function () {
+    this.setData({
+      isLogin: true
+    })
+  },
+  // 发送验证码
+  sendVerify:function(){
+    let me=this;
+    if (this.data.sending){
+      this.setData({
+        sending: false
+      });
+      let text=61;
+      let sendInt=setInterval(() => {
+        if (text>1){
+          text--
+          me.setData({
+            verifyButtenText: text
+          })
+        }else{
+          clearInterval(sendInt)
+          me.setData({
+            verifyButtenText: '发送验证码',
+            sending:true
+          })
+        }
+      }, 1000);
+
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -137,7 +280,12 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    let position= wx.getMenuButtonBoundingClientRect()
+    console.log(wx.getMenuButtonBoundingClientRect())
+    this.setData({
+      topVal: position.top,
+      heightVal: position.height-10
+    })
   },
 
   /**
